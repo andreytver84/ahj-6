cd6d677614c881589fc824e34fdc9ecb6096654b
Андрей Увиков <uvikmedium@mail.ru>
2023-02-04 20:11:53 +0300

    add html and some js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,19 @@
+const allCols = document.querySelectorAll('.col');
 
+const toggleDisplay = function(b) {
+    const parentCol = b.closest('.col');
+    parentCol.querySelector('.textarea-block').classList.toggle("hidden");
+    parentCol.querySelector('.col-addbtn').classList.toggle("hidden");
+}
+const addTask = function(btn) {
+    const textArea = btn.closest('.textarea-block').querySelector('textarea');
+    const text = textArea.value;
+    console.log(text);
+}
+allCols.forEach(item => item.addEventListener('click', (e) => {
+    if (e.target.classList.contains('col-addbtn') || e.target.classList.contains('close-add-card')) {
+        toggleDisplay(item);
+    } else if (e.target.classList.contains('add-card')) {
+        addTask(e.target);
+    }
+}));
